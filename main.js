8f42197ff9b51a2d330f8af46997a0f69faff15f
Elvis Wei <ewei068@berkeley.edu>
2020-11-11 16:32:41 -0600

    modified node

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 app.use("/scripts",  express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+app.listen(3000, () => console.log('Gator app listening on port 300!'));
